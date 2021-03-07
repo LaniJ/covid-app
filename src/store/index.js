1f9418b0e48a40942f35045ed00e4e8632549ded
Lani Oluwajuyitan <ooluwalani@gmail.com>
2021-03-08 00:12:13 +0100

    working version of covid-19 app

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,97 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    rawData: {},
+    totalCasesWordwide: 0,
+    activeCasesWordwide: 0,
+    totalRecoveriesWordwide: 0,
+    totalDeathsWordwide: 0,
+    top3Countries: [],
+    totalCasesPerRegion: {
+      Europe: 0, 'North America': 0, 'South America': 0, Asia: 0, Africa: 0, Oceania: 0,
+    },
+    allCountries: []
   },
   mutations: {
+    setRawData: (state, payload) => { state.rawData = payload; },
+    setTotalCasesWorldwide: (state, payload) => { state.totalCasesWordwide = payload; },
+    setActiveCasesWorldwide: (state, payload) => { state.activeCasesWordwide = payload; },
+    setTotalRecoveriesWordwide: (state, payload) => { state.totalRecoveriesWordwide = payload; },
+    setTotalDeathsWordwide: (state, payload) => { state.totalDeathsWordwide = payload; },
+    setTop3Countries: (state, payload) => { state.top3Countries = payload; },
+    setTotalCasesPerRegion: (state, payload) => { state.totalCasesPerRegion = payload },
+    setAllCountries: (state, payload) => { state.allCountries = payload }
   },
   actions: {
+    fetchCasesByContinent:({ commit, state }, payload) => {
+      const apiURL = 'https://covid-api.mmediagroup.fr/v1/cases'
+      const continentNames = Object.keys(state.totalCasesPerRegion)
+      const promises = continentNames.map( continent => fetch(`${apiURL}?continent=${continent}`))
+      Promise.all(promises)
+      .then((responses)=> {
+        const jsonPromises = responses.map(response => response.json())
+        return Promise.all(jsonPromises)
+      }).then((continentDataList) => {
+        const casesPerRegion = {}
+        continentNames.forEach((continent, index) => {  
+          const continentData = continentDataList[index]
+          let totalCases = 0
+
+          Object.keys(continentData).forEach((country) => {
+            const countryData = continentData[country].All
+            totalCases += countryData.confirmed
+          })
+          casesPerRegion[continent] = totalCases
+        })
+        commit('setTotalCasesPerRegion', casesPerRegion);
+
+        console.log(continentDataList);
+        console.log(casesPerRegion);
+      })
+    },
+
+    fetchCases: ({ commit, state }, payload) => {
+      const apiURL = 'https://covid-api.mmediagroup.fr/v1/cases'
+      fetch(apiURL)
+      .then((response) => response.json())      
+      .then((rawData) => {
+
+        const dataAsList = []
+
+        Object.keys(rawData).forEach((country) => {
+          if (country == 'Global') {
+            return 
+          }
+          const countryData = rawData[country].All
+          dataAsList.push(countryData)
+          
+        })
+        // sort in decending order
+        dataAsList.sort((countryData1, countryData2) => {
+          return countryData2.confirmed - countryData1.confirmed
+        })
+        
+        let activeCases = rawData.Global.All.confirmed - rawData.Global.All.deaths - rawData.Global.All.recovered
+        // state.rawData.all
+        commit('setRawData', rawData);
+        commit('setTotalCasesWorldwide', rawData.Global.All.confirmed);
+        commit('setActiveCasesWorldwide', activeCases)
+        commit('setTotalRecoveriesWordwide', rawData.Global.All.recovered)
+        commit('setTotalDeathsWordwide', rawData.Global.All.deaths)
+        commit('setTop3Countries', [dataAsList[0], dataAsList[1], dataAsList[2]])
+        commit('setAllCountries', dataAsList)
+      })
+    }
+  },
+  getters: {
+    allCovidData: (state) => state.rawData,
+    totalCases: (state) => state.totalCasesWordwide,
+    activeCases: (state) => state.activeCasesWordwide,
+    recoveries: (state) => state.totalRecoveriesWordwide,
+    deaths: (state) => state.totalDeathsWordwide,
+    top3Countries: (state) => state.top3Countries,
+    regionalCases: (state) => state.totalCasesPerRegion,
+    allCountries: (state) => state.allCountries
   },
   modules: {
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <h3> {{ allCovidData }}</h3> -->
+    <div class="loader" v-if="isLoading">
+        Loading...
+    </div>
+    <div class="news-grid" v-else>
+      <h2>Covid Statistics (Worldwide)</h2>
+      <p> Total Covid cases: {{ totalCases}} </p>
+      <p> Active cases: {{activeCases}} </p>
+      <p> total recoveries are: {{ recoveries }} </p>
+      <p> total deaths are: {{ deaths }} </p>
+
+      <div>
+        <h3>Top Affected Countries</h3>
+        <p v-for="item in top3Countries" :key="item.country">
+          {{ item.country }} : {{ item.confirmed}}
+        </p>
+      </div>
+      
+      <div>
+        <h2>Total number of cases per region</h2>
+        <h3> Africa: {{ regionalCases["Africa"] }} </h3>
+        <h3> Asia: {{ regionalCases["Asia"] }} </h3>
+        <h3> Europe: {{ regionalCases["Europe"] }} </h3>
+        <h3> North America: {{ regionalCases["North America"] }} </h3>
+        <h3> South America: {{ regionalCases["South America"] }} </h3>
+        <h3> Oceania: {{ regionalCases["Oceania"] }} </h3>
+      </div>
+
+      <div>
+        <table>
+          <tr>
+            <th>Country</th>
+            <th>Total Cases</th>
+            <th>Active Cases</th>
+            <th>Recoveries</th>
+            <th>Deaths</th>
+          </tr>
+          <tr v-for="item in allCountries" :key="item.country">
+            <td>{{ item.country }}</td>
+            <td>{{ item.confirmed }}</td>
+            <td>{{ item.confirmed - item.recovered - item.deaths }}</td>
+            <td>{{ item.recovered }}</td>
+            <td>{{ item.deaths }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { mapActions, mapGetters } from 'vuex';
 
 export default {
+
   name: 'Home',
-  components: {
-    HelloWorld,
+  data: () => ({
+    isLoading: true,
+  }),
+  watch: {
+    allCovidData: {
+      deep: true,
+      handler() {
+        this.isLoading = false;
+      },
+    },
+  },
+  mounted() {
+    this.fetchCases();
+    this.fetchCasesByContinent()
+  },
+  computed: {
+    ...mapGetters(['allCovidData', 'totalCases', 'activeCases', 'recoveries', 'deaths', 'top3Countries', 'regionalCases', 'allCountries']),
+  },
+  methods: {
+    ...mapActions(['fetchCases', 'fetchCasesByContinent']),
   },
 };
 </script>
